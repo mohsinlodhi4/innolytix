a894a9265d6ab56af3f7bf6891cafcbdfa72b3d8
Mohsin Khan Lodhi <mohsinlodhi4@gmail.com>
2022-05-10 09:26:34 -0700

    changes

---
+++ app/helper.php
@@ -0,0 +1,74 @@
+<?php
+
+if (! function_exists('datatables')) {
+    /**
+     * Helper to make a new DataTable instance from source.
+     * Or return the factory if source is not set.
+     *
+     * @param  mixed  $source
+     * @return \Yajra\DataTables\DataTableAbstract|\Yajra\DataTables\DataTables
+     */
+    function datatables($source = null)
+    {
+        if (is_null($source)) {
+            return app('datatables');
+        }
+
+        return app('datatables')->make($source);
+    }
+}
+
+function removeTwoZeroes($val){
+    return (float)substr($val,0, strlen($val)-2);
+}
+
+function random_strings($length_of_string=6)
+{
+ 
+    // String of all alphanumeric character
+    $str_result = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+ 
+    // Shuffle the $str_result and returns substring
+    // of specified length
+    return substr(str_shuffle($str_result),
+                       0, $length_of_string);
+}
+
+
+function amountInWords(float $amount)
+{
+   $amount_after_decimal = round($amount - ($num = floor($amount)), 2) * 100;
+   // Check if there is any number after decimal
+   $amt_hundred = null;
+   $count_length = strlen($num);
+   $x = 0;
+   $string = array();
+   $change_words = array(0 => '', 1 => 'One', 2 => 'Two',
+     3 => 'Three', 4 => 'Four', 5 => 'Five', 6 => 'Six',
+     7 => 'Seven', 8 => 'Eight', 9 => 'Nine',
+     10 => 'Ten', 11 => 'Eleven', 12 => 'Twelve',
+     13 => 'Thirteen', 14 => 'Fourteen', 15 => 'Fifteen',
+     16 => 'Sixteen', 17 => 'Seventeen', 18 => 'Eighteen',
+     19 => 'Nineteen', 20 => 'Twenty', 30 => 'Thirty',
+     40 => 'Forty', 50 => 'Fifty', 60 => 'Sixty',
+     70 => 'Seventy', 80 => 'Eighty', 90 => 'Ninety');
+    $here_digits = array('', 'Hundred','Thousand','Lakh', 'Crore');
+    while( $x < $count_length ) {
+      $get_divider = ($x == 2) ? 10 : 100;
+      $amount = floor($num % $get_divider);
+      $num = floor($num / $get_divider);
+      $x += $get_divider == 10 ? 1 : 2;
+      if ($amount) {
+       $add_plural = (($counter = count($string)) && $amount > 9) ? 's' : null;
+       $amt_hundred = ($counter == 1 && $string[0]) ? ' and ' : null;
+       $string [] = ($amount < 21) ? $change_words[$amount].' '. $here_digits[$counter]. $add_plural.' 
+       '.$amt_hundred:$change_words[floor($amount / 10) * 10].' '.$change_words[$amount % 10]. ' 
+       '.$here_digits[$counter].$add_plural.' '.$amt_hundred;
+        }
+   else $string[] = null;
+   }
+   $implode_to_Rupees = implode('', array_reverse($string));
+   $get_paise = ($amount_after_decimal > 0) ? "And " . ($change_words[$amount_after_decimal / 10] . " 
+   " . $change_words[$amount_after_decimal % 10]) . ' Paise' : '';
+   return ($implode_to_Rupees ? $implode_to_Rupees . 'Rupees ' : '') . $get_paise;
+}
